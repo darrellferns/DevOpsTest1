92f06f5f1bc5f2ffbd0513328511cbc9d3b2b6f8
darrellferns <49779977+darrellferns@users.noreply.github.com>
2019-04-23 18:02:24 +0530

    Test
    Test

--- TestConsoleProject/TestConsoleProject/Module1.vb
+++ TestConsoleProject/TestConsoleProject/Module1.vb
@@ -1,7 +1,7 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Okay its done... Lets try it again.")
+        Console.WriteLine("Okay its done... Lets try it again. and again")
         Console.ReadLine()
     End Sub
 
