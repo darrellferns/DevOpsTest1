30cf6f9103472f2bab973d0917fb13a97c3fa6cc
darrellferns <49779977+darrellferns@users.noreply.github.com>
2019-04-23 15:52:57 +0530

    Yes

--- TestConsoleProject/TestConsoleProject/Module1.vb
+++ TestConsoleProject/TestConsoleProject/Module1.vb
@@ -1,7 +1,7 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.WriteLine("Will this work No Shit.... Or will it?")
+        Console.WriteLine("Is Dipti in Love with ....... ?")
         Console.ReadLine()
     End Sub
 
